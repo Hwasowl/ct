0cf687ca94d1c0b6c2f595dd96baa4db40095992
Jungsoo Park <44762533+Hwasowl@users.noreply.github.com>
2025-08-25 10:19:43 +0900

    [level 2] Title: 행렬의 곱셈, Time: 11.08 ms, Memory: 101 MB -BaekjoonHub

---
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length];
+        // 행
+        for (int i = 0; i < arr1.length; i++) {
+            // 열
+            for (int j = 0; j < arr2[0].length; j++) {
+                // 행열곱
+                for (int z = 0; z < arr2.length; z++) {
+                    answer[i][j] += arr1[i][z] * arr2[z][j];       
+                }
+            }
+        }
+        return answer;
+    }
+}
