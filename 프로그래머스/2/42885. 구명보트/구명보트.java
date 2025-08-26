57508f8eccec17b0472a5a2ffc403d1cf6121681
Jungsoo Park <44762533+Hwasowl@users.noreply.github.com>
2025-08-26 21:33:25 +0900

    [level 2] Title: 구명보트, Time: 10.89 ms, Memory: 58 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -17,6 +17,12 @@ class Solution {
         // 50 70 X 보트+1 right-- (0, 1)
         // 50 50 O 보트+1 left-- right-- (1, 0)
         
+        // 리밋240
+        // 80 90 120 120 120
+        // 80 120 O 보트+1 left++ right-- (1, 3)
+        // 90 120 O 보트+1 left++ righ-- (2, 2)
+        // 120 보트+1 right-- (2, 1)
+        
         return answer;
     }
 }
