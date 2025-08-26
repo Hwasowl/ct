30bf9b65f3a0cc56b31527fe3215d35c1ca6accc
Jungsoo Park <44762533+Hwasowl@users.noreply.github.com>
2025-08-26 21:30:09 +0900

    [level 2] Title: 구명보트, Time: 12.05 ms, Memory: 58.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        int left = 0; int right = people.length-1;
+        
+        while(left <= right) {
+            if (people[left] + people[right] <= limit) left++;
+            right--;
+            answer++;
+        }
+        
+        // 50 50 70 80
+        // 50 80 X 보트+1 right-- (0, 2)
+        // 50 70 X 보트+1 right-- (0, 1)
+        // 50 50 O 보트+1 left-- right-- (1, 0)
+        
+        return answer;
+    }
+}
