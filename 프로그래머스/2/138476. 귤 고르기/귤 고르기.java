af9eae2fd74a78f9a9694cf219f2d9c0c5d5bfc3
Jungsoo Park <44762533+Hwasowl@users.noreply.github.com>
2025-08-28 13:05:09 +0900

    [level 2] Title: 귤 고르기, Time: 61.15 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,35 +1,21 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int k, int[] tangerine) {   
-        Arrays.sort(tangerine);
-        Set<Integer> set = new LinkedHashSet<>();
-        for(int t : tangerine) set.add(t);
-        List<Integer> li = new ArrayList<>(set);
-        int prevIdx = 0;
-        
-        List<Integer> counts = new ArrayList<>();
-        for (int i = 0; i < li.size(); i++) {
-            int cnt = 0;
-            int result = li.get(i);
-            for(int j = prevIdx; j < tangerine.length; j++) {
-                if (result == tangerine[j]) cnt++;
-                else {
-                    prevIdx = j;
-                    break;
-                };
-            }
-            counts.add(cnt);
-       }
-        
-        Collections.sort(counts, Collections.reverseOrder());
+    public int solution(int k, int[] tangerine) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        for (int t : tangerine) {
+            countMap.put(t, countMap.getOrDefault(t, 0) + 1);
+        }
+
+        List<Integer> counts = new ArrayList<>(countMap.values());
+        counts.sort(Collections.reverseOrder());
 
         int sum = 0;
         int answer = 0;
-        for(Integer c : counts) {
+        for (int c : counts) {
+            sum += c;
             answer++;
-            sum+=c;
-            if(sum >= k) break;
+            if (sum >= k) break;
         }
 
         return answer;
