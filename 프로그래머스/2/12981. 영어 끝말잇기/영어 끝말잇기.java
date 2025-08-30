918e986a4ac0b90a7a8eaf200e8336f745b7b709
Jungsoo Park <44762533+Hwasowl@users.noreply.github.com>
2025-08-30 14:35:11 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.31 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -2,17 +2,16 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n, String[] words) {
-        List<String> li = new ArrayList<>();
-        li.add(words[0]);
+        Set<String> st = new HashSet<>(); st.add(words[0]);
         int idx = -1;
         for(int i = 1; i < words.length; i++) {
             String prev = words[i-1].substring(words[i-1].length()-1);
             String curr = words[i].substring(0, 1);
-            if (!curr.equals(prev) || li.contains(words[i])) {
-                idx = i;
-                break;
+            int lss = st.size();
+            st.add(words[i]);
+            if (!curr.equals(prev) || lss == st.size()) {
+                idx = i; break;
             }
-            li.add(words[i]);
         }
         if (idx == -1) return new int[]{0, 0};
         return new int[]{(idx%n)+1, (idx/n)+1};
