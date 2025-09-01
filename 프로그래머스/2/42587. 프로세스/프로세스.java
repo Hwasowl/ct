54e028c93ea2eb9073394f185421ab5eb5c6af8c
Jungsoo Park <44762533+Hwasowl@users.noreply.github.com>
2025-09-02 08:51:54 +0900

    [level 2] Title: 프로세스, Time: 1.37 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,16 +1,18 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[] priorities, int location) {
+    public int solution(int[] priorities, int location) { // 복습
         int answer = 0;
-        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
-        for (int num : priorities) queue.add(num);
-        while(!queue.isEmpty()) {
-            for (int i = 0; i < priorities.length; i++) {
-                if (priorities[i] == queue.peek()) {
-                    queue.poll();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder()); // 3 2 2 1 순서.
+        for(int p : priorities) pq.add(p);
+        while(!(pq.size() == 0)) {
+            // while안에 for문을 둠으로써 배열 크기를 초과하고, 초기로 돌아오는 로직을 수행.
+            // for문 안에는 if i == location으로 비교해 인덱스 문제를 해결한다.
+            for(int i = 0; i < priorities.length; i++) {
+                if (priorities[i] == pq.peek()) { // 우선순위와 같다면
                     answer++;
-                    if (i == location) return answer;
+                    pq.poll();
+                    if (i == location) return answer; // 우선순위가 같으면서 위치가 같다면 반환.
                 }
             }
         }
