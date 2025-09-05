4644d5a9a6c4ca5628b4f363d31a35db7c03696a
Jungsoo Park <44762533+Hwasowl@users.noreply.github.com>
2025-09-05 12:41:23 +0900

    [level 2] Title: 주식가격, Time: 15.71 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        for(int i = 0; i < prices.length; i++) {
+            int time = 0;
+            int price = prices[i];
+            // 가격이 1인 경우 떨어지는 일이 없기에 바로 반환
+            if (price == 1) {
+                answer[i] = (prices.length-i)-1;
+                continue;
+            }
+            // 가격이 2 이상인 경우 자신보다 낮은 값을 찾아 반환
+            for(int j = i+1; j < prices.length; j++) {
+                time++;
+                if (price > prices[j]) {
+                    answer[i] = time;
+                    time = 0;
+                    break;
+                }
+            }
+            if (time != 0) answer[i] = (prices.length-i)-1;
+        }
+        return answer;
+    }
+}
